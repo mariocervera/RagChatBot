395c947a7ecc90bd688572678d9b801c6322e18c
Mario Cervera <mariocervera@gmail.com>
2025-09-20 01:01:44 +0200

    remove unnecessary new lines

--- src/main.js
+++ src/main.js
@@ -4,10 +4,8 @@ import { generateEmbeddingsForChunks } from './embeddings.js';
 import { storeEmbeddingsInVectorDatabase } from './database.js';
 import { respondUsingTool } from './chat.js';
 
-
 const contextFile = './resources/context.txt';
 const query = "What is my profession?"
-
 
 async function main() {
     try {
